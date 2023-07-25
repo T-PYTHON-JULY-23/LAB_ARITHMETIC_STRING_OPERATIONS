4a7c276cf0278d25d027cc946904ebf94524eed6
MaramFahad <maramll98ll@gmail.com>
2023-07-25 20:06:46 +0300

    BonusLab

---
+++ BonusLab.py
@@ -0,0 +1,10 @@
+about_me="my name is Maram Fahad AlHmoud and iam 25 years old"
+name="Maram"
+print(len(about_me))
+print(about_me.index(name))
+print(about_me.count(name))
+print(about_me.upper())
+print(about_me.lower())
+print(about_me.replace(name, "Deem",))
+print(about_me[-1])
+
