09fb402ca984cd86b97dfd0d32c43e2a8b59196d
Lenah11 <lenhh11@gmail.com>
2023-07-25 17:05:47 +0300

    Bpnus_Lenah

---
+++ Bonus.py
@@ -0,0 +1,27 @@
+#Define a string variable containing a sentence with at least 10 words.
+phrase_10_words = "Hello my name is lenah"
+
+#Define a string variable containing a word that appears in the sentence.
+Str_appears = "Hello"
+
+#Print the length of the sentence.
+print(len(phrase_10_words))
+
+#Print the index of the first occurrence of the word in the sentence.
+print(phrase_10_words.index("Hello"))
+
+#Print the number of times the word appears in the sentence.
+print(phrase_10_words.count("name"))
+
+#Print the sentence in all uppercase letters.
+print(phrase_10_words.upper())
+
+#Print the sentence in all lowercase letters.
+print(phrase_10_words.lower())
+
+#Replace the word in the sentence with a new word of your choice.
+print(phrase_10_words.replace("Hello", "Hi"))
+
+#Print the last character of the sentence.
+print(phrase_10_words[21])
+
