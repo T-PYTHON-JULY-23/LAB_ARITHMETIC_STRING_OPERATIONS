4c4974ee21a0e69d854f8e580b3e02051fa553a8
naif <naif.albariqi201@hotmail.com>
2023-07-25 20:16:12 +0300

    Bouns - SOL

---
+++ Bonus.py
@@ -0,0 +1,10 @@
+import time
+var1 = "I love my team Real Madrid"
+print(f"split :", var1.split())
+print(f"The Length : {len(var1)}")
+print(f"First char : ", var1.index("I"))
+print(f"The number of times :", var1.count("m"))
+print(f"Upper : ", var1.upper())
+print(f"Lower : ", var1.lower())
+print(f"Replace : ", var1.replace("I love my team Real Madrid","hello world"))
+print(f"Last Char : ", var1[-1])
