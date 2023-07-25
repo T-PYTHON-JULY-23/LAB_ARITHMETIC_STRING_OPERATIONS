74a3c284aef9eb1cf8fa37eeeeb5d9fbfb6bd9a8
Futon <Futonalqahtani>
2023-07-26 02:39:55 +0300

    Done2

---
+++ python3.py
@@ -0,0 +1,14 @@
+word_12 = "always remember taht If you Don't belong don't be long"
+word_11 = "belong"
+print(len(word_12))
+y = word_12.index("always")
+print(y)
+x = word_12.count("don't")
+print(x)
+f = word_12.upper()
+print(f)
+A = word_12.lower()
+print(A)
+M= word_12.replace("that","this")
+print(M)
+print(word_12[-1])
