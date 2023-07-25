23eb46209315a8433135f9c0f0808fca063d7226
ghaida <ghaida@gmail.com>
2023-07-26 01:55:25 +0300

    bouns

---
+++ bouns.py
@@ -0,0 +1,12 @@
+
+
+
+wisdom="However difficult life may seem, there is always something you can do and succeed at"
+
+word="difficult"
+
+print("length of the sentence",len(wisdom))
+print(" the number of times the word appears in the sentence",wisdom.count(word))
+print("uppercase letters",wisdom.upper())
+print("lowercase letters",wisdom.lower())
+print("last character of the sentence",wisdom[-1])
