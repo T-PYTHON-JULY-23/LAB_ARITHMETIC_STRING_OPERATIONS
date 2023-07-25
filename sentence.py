83dfdd453a91a8f70ce62bec664c380b2257a38d
Nada <Nada@gmail.com>
2023-07-25 23:39:46 +0300

    sntence

---
+++ sentence.py
@@ -0,0 +1,11 @@
+name_city = "Hello my name is Nada Musaad Almutairi i'm from Riyadh"
+second_name = "Musaad"
+print(len(name_city))
+print(name_city.index("from"))
+print(len(name_city.split()))
+print(name_city.upper())
+print(name_city.lower())
+print(name_city.replace("Riyadh" , "khobar"))
+print(name_city[-1])
+
+
