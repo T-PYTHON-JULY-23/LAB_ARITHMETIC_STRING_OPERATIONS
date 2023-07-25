979a386140ece942276c9ddd88d6b0b16029545a
sarahnaghi <sara.naghi@gmail.com>
2023-07-25 19:40:28 +0300

    lab work done

---
+++ bouns.py
@@ -0,0 +1,15 @@
+fullName= "I am Sarah Haitham Naghi I love studying python at tuwaiq academy"
+lastName = "Naghi"
+
+
+print(len(fullName))
+print(fullName.index("Naghi"))
+print(fullName.count("Naghi"))
+
+print(fullName.upper())
+print(fullName.lower())
+
+replace= lastName.replace("Naghi", "Na")
+
+print(replace[-1])
+print(fullName[-1])
