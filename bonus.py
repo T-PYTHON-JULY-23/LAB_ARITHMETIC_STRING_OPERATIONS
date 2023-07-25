244f0692b37cb93e1802db3658f4d8e3c91a529f
suha <suha.alrajhi01@gmail.com>
2023-07-25 23:22:26 +0300

    BONUS suha aziz

---
+++ bonus.py
@@ -0,0 +1,12 @@
+# bonus lab
+
+qoutes = "Keep your face always toward the sunshine and the shadows will fall behind you"
+word = "shadows"
+print("the lenght of the sentence: ", len(qoutes))
+print(qoutes.index(word))
+print(qoutes.count(word))
+print(qoutes.upper())
+print(qoutes.lower())
+print(qoutes.replace("sunshine" , "sun"))
+lastchar = qoutes[-1]
+print(lastchar)
