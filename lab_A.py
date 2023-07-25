88823579736938496e9274b653487e2aec60b1bd
nada <nada_abdullah_8@hotmail.com>
2023-07-25 21:08:25 +0300

    savee

--- lab_A.py
+++ lab_A.py
@@ -13,7 +13,7 @@ Subtotal = price * Quantity
 tax = Subtotal * tax_rate /100
 
 Total = Subtotal + tax 
-x=tax
+
 
 print (f"Price of item: {price}$\nQuantity: {Quantity}\nTax rate: {tax_rate}% ")
 print (f"\n\nSubtotal: {Subtotal}$\nTax: {tax:.2}$\nTotal: {Total:.2}$")
