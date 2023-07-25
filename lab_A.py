7490bb23db3276fb458d07887e09d30f6fe44502
nada <nada_abdullah_8@hotmail.com>
2023-07-25 21:05:50 +0300

    save

---
+++ lab_A.py
@@ -0,0 +1,19 @@
+
+
+
+
+price  =2.99
+
+Quantity= 3 
+
+tax_rate= 7.5 
+
+Subtotal = price * Quantity
+
+tax = Subtotal * tax_rate /100
+
+Total = Subtotal + tax 
+x=tax
+
+print (f"Price of item: {price}$\nQuantity: {Quantity}\nTax rate: {tax_rate}% ")
+print (f"\n\nSubtotal: {Subtotal}$\nTax: {tax:.2}$\nTotal: {Total:.2}$")
