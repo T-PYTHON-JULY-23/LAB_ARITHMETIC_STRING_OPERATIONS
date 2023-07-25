2af9f83ada7910ffeec608500eab89a9485c1360
Basma <129796403+basiix1@users.noreply.github.com>
2023-07-25 16:34:28 +0300

    bouns - basma

---
+++ Bonus.py
@@ -0,0 +1,21 @@
+## Bonus (Not required), create a new python file and do the following:
+
+#- Define a string variable containing a sentence with at least 10 words.
+St= "My name is basma al shahrani i love python"
+print (St)
+#- Define a string variable containing a word that appears in the sentence.
+Word = "name"
+#- Print the length of the sentence.
+print(len(St))
+#- Print the index of the first occurrence of the word in the sentence.
+print(St.index(Word))
+#- Print the number of times the word appears in the sentence.
+print(St.count(Word))
+#- Print the sentence in all uppercase letters.
+print(St.upper())
+#- Print the sentence in all lowercase letters.
+print(St.lower())
+#- Replace the word in the sentence with a new word of your choice.
+print(St.replace(Word , "new1"))
+#- Print the last character of the sentence.
+print(St[-1])
