423b7ffd84dd359c2bff0fa0e03a902493f9c629
MaramFahad <maramll98ll@gmail.com>
2023-07-26 14:21:30 +0300

    edit the taxes equation

--- ArithmeticLab.py
+++ ArithmeticLab.py
@@ -2,16 +2,15 @@
 #purchase invoice 
 price= 5.75
 quantity= 4
-tax_rate= 0.15
+tax_rate= 15
 subtotal = price*quantity
-tax= subtotal*tax_rate
+tax= subtotal*(tax_rate/100)
 total= subtotal+tax
 
-print("Price of item: $", price)
+print(f"Price of item: ${price}") #استخدامه عشان استغني عن الفواصل بين النص والمتغير
 print("Quantity: ",quantity)
-print("Tax rate: ", tax_rate*100, "%")
+print("Tax rate:", tax_rate, "%")
 print('\n' )
 print("Subtotal: $",subtotal)
 print("Tax: $",round(tax,2))
 print("Total: $", total)
-
