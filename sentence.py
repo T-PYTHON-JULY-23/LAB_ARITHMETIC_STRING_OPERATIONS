b761b9073a183d07085dae7eae17d9eba7abe034
Nada <Nada@gmail.com>
2023-07-26 14:37:03 +0300

    store file

--- sentence.py
+++ sentence.py
@@ -1,11 +1,1 @@
-name_city = "Hello my name is Nada Musaad Almutairi i'm from Riyadh"
-second_name = "Musaad"
-print(len(name_city))
-print(name_city.index("from"))
-print(len(name_city.split()))
-print(name_city.upper())
-print(name_city.lower())
-print(name_city.replace("Riyadh" , "khobar"))
-print(name_city[-1])
-
-
+hight = float(input("your height in m : "))
