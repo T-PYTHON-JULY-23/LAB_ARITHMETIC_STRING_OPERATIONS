59a39b873bd7f0f350fa3cc08fe99f5b7e791e05
Lama Alsharif <lama.a.alsharif@gmail.com>
2023-07-26 08:58:19 +0300

    the lab solved

---
+++ String_operation.py
@@ -0,0 +1,11 @@
+var1 = "Iam Lama Alsharif i am from saudi arabia , I had just finished my bachelor degree in computer science"
+var2 = "Lama"
+print(len(var1))
+print (var1.count(""))
+print(var1.index("Lama"))
+print(var1.count("Lama"))
+print (var1.upper())
+print (var1.lower())
+print(var1[-1])
+replaced_word = var1.replace('Alsharif' , 'Abdullah')
+print(replaced_word)
